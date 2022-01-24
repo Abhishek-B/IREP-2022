8ecc62591bf7e6fd93ab825a820e9d1a98651800
Abhishek Bhardwaj <ab2824484@gmail.com>
2022-01-24 16:41:23 +1100

    next update
    Next update will have a redesign, where the convex subproblem is solved with Yalmip+Mosek.
    Matlab's fminsearch seems very broken.

--- Omega_n.m
+++ Omega_n.m
@@ -1,6 +1,7 @@
-function [price] = Omega_n(p_n,eta,kappa_n)
+function [price] = Omega_n(u_n,T,eta,kappa_n)
 %Returns the price of electricity given the parameters and power use
 
+p_n = u_n(1:T);
 price = eta'*p_n + kappa_n*(p_n'*p_n);
 
 end
