0ff607b4303c6b288b0a9baa20be96f724eb26d4
Abhishek Bhardwaj <ab2824484@gmail.com>
2022-01-24 23:42:58 +1100

    update
    Can't use YALMIP, since one of the constraints needs mu(i), which depends on the sign of the variable p(i).
    So, very likely need to do this numerically for it to work - i.e., YALMIP's out.

--- IEEE13Node_Example.m
+++ IEEE13Node_Example.m
@@ -11,6 +11,7 @@ T = 48;
 K = 29;
 
 % power profile vectors (empty for now)
+% maybe I dont need these here...eh whatever, I'll come back to it
 p = zeros(T,N);
 q = zeros(T,N);
 s = zeros(2*K*T, N);
@@ -20,14 +21,24 @@ u = [p;q;s];
 % customer i
 % setting to zero for now, will fill in soon
 
-a = zeros(1,N); % arrival time
-d = zeros(1,N); % departure time
-b = zeros(1,N); % battery capacity - this isn't used in the manuscript anywhere
-s_bar = zeros(1,N); % inverter capacity
+a = randi([10,14],1,N); % arrival time 5pm-7pm
+d = randi([38,42],1,N); % departure time 7am-9am
+
+capacities = [42,21,24,20,30,16,23,16.5,28,60,90,75];
+b = zeros(1,N); 
+for i=1:N
+    b(i) = capacities(randi([1,12]));
+end
+s_bar = 1000*ones(1,N); % inverter capacity
 sig_hat = (0.3+0.2*rand(1,N)).*b; % initial SoC
-sig_star = zeros(1,N); % target SoC
 sig_u = 0.85*b; % max SoC
 sig_l = 0.2*b;  % min SoC
+sig_star = zeros(1,N); % target SoC - this isnt listed in the paper, so I 
+                       % will set this to between sig_hat and sig_upper,
+                       % rounded to the nearest integer
+for i=1:N
+    sig_star(i) = ceil(sig_hat(i) + (sig_u(i)-sig_hat(i))*rand);
+end
 p_u = 6.6;  % max charge rate
 p_l = -6.6; % min charge rate
 mu_u = 0.9; % charge efficiency
@@ -43,6 +54,11 @@ e = sig_star - sig_hat;
 
 % price column vector
 eta = zeros(T,1);
+eta(1:4) = 0.23;
+eta(5:16)=0.54;
+eta(17:20) = 0.23;
+eta(21:38) = 0.15;
+eta(39:48) = 0.23;
 
 % Regularization paramters for cost
 kappa = 5e-4; %zeros(1,N);
@@ -150,12 +166,14 @@ end
 
 costs={};
 for i=1:N
-    costs{i} = @(u) Omega_n(u, T, eta, kappa); % + Indicator_n(u, T, w, s_bar(i), mu_u, mu_l, a(i), d(i), p_u, p_l, alpha_u(i), alpha_l(i), e(i));
+    costs{i} = @(u) eta'*u(1:T) + kappa*u(1:T)'*u(1:T); %Omega_n(u, T, eta, kappa); % + Indicator_n(u, T, w, s_bar(i), mu_u, mu_l, a(i), d(i), p_u, p_l, alpha_u(i), alpha_l(i), e(i));
 end
+
+% Do i really need this crap if I'm using YALMIP?...
 
 %% ADMM
 
-iteration = 1000;
+iteration = 10;
 lambda = zeros(2*K*T,N);       % primal value for each customer
 lambda_state = zeros(2*K*T,N); % state value for each customer, the neighbors states
                       % can be found from different columns of this.
@@ -170,8 +188,8 @@ transmission = zeros(N,iteration);
 
 for k=1:iteration
     
-    % Update of the primal variable
-    for i=1:N
+    % looping over customers
+    for i=1:1
         % Building the sum of difference between self state and neighbors
         % states
         temp=0;
@@ -179,20 +197,106 @@ for k=1:iteration
             temp = temp +( lambda_state(:,i) + lambda_state(:,neighbors{i}(j)) );
         end
         
+        %% Updating u_n for the primal step
+        % Using YALMIP for this one
+        
+        %% Setup of SDPVARS
+        % each customers u_n
+        x = sdpvar(2*(T+K*T),1);
+        
+        % finding p,q,s from u
+        p = x(1:T);
+        q = x(T+1:2*T);
+        s = x(2*T+1:end);
+        
+        % sanity check
+        if ~size(p,1)==size(q,1)
+            error("sizes don't match for p and q")
+        end
+        
+        %% constraints - inverter capacity
+        
+        constraints = [];
+        inv_cap = s_bar(i)^2;
+        for j=1:T
+            constraints = [constraints, p(j)^2+q(j)^2<= inv_cap];
+        end
+        
+        %% Constraints building other stuff
+        
+        % Building vector mu, of charge efficiencies
+        mu = zeros(T,1);
+        for j=1:T
+            if p(j)>=0
+                mu(j)=mu_u;
+            else
+                mu(j)=mu_l;
+            end
+        end
+
+        % Building matrix M which bounds charge rates
+        M = zeros(T,T);
+        for j=1:T
+            for l=1:T
+                if j>=l
+                    M(j,l) = mu(l);
+                else
+                    M(j,l)=0;
+                end
+            end
+        end
+
+        % Building availibility matrix L
+        L = zeros(T,T);
+        for j=1:T
+            for l=1:T
+                if (j==l)&&(a(i)<j<=d(i))
+                    L(j,l)=1;
+                end
+            end
+        end
+        
+        I = eye(T,T);
+        O = ones(T,1);
+        Z = zeros(T,1);
+        
+        F = [I ; -I ; M ; -M ; mu' ; -mu' ; (I-L) ; (L-I)];
+        f = [p_l*O ; -p_u*O ; alpha_l(i)*O ; -alpha_u(i)*O ; e(i) ; -e(i) ; Z ; Z];
+         
+        %% Constraints - final constraint for feasibility set
+        
+        constraints = [constraints, F*p>=f];
+        
+        %% Updating u_n with optimize and Mosek
+        
+        obj = eta'*p + kappa*p'*p;
+        
+        optimize(constraints, obj);
+        u = value(x);
         
         
-        
-        % the overall cost function for the primal update
-        fun = @(x)costs{i}(x) + x'*(lambda(:,i) - c*temp) + c*n_deg(i)*norm(x)^2;
-        
-        % updating the primal through a subminimization
-        lambda(:,i) = fminsearch(fun, zeros(p,1));
+        %% Updating lambda
         
         
+        lambda(:,i) = (1/(2*c*n_deg(i)))*( Psi{i}*u - (1/N)*w - nu(:,i) + c*temp );
         
+%         
+%         % the overall cost function for the primal update
+%         fun = @(x)costs{i}(x) + x'*(lambda(:,i) - c*temp) + c*n_deg(i)*norm(x)^2;
+%         
+%         
+%         
+%         
+%         
+%         
+%         
+%         
+%         %%
+%         % updating the primal through a subminimization
+%         lambda(:,i) = fminsearch(fun, zeros(p,1));
+%         
         
-        
-        
+        %% Transmission Step
         % Computing the difference between current state and primal update
         xi(:,i) = lambda_state(:,i) - lambda(:,i); 
         %disp(xi(:,i)')
@@ -207,7 +311,7 @@ for k=1:iteration
         end
     end
     
-    % Dual variable update
+    %% Dual variable update
     for i=1:n
         
         % building the sum of the differences between self state and
@@ -221,46 +325,12 @@ for k=1:iteration
         nu(:,i) = nu(:,i) + c*temp2;
     end
     
-    % Storing the primal and dual updates for the iteration
+    %% Storing the primal and dual updates for the iteration
     lambda_step{k} = lambda;
     nu_step{k} = nu;
 end
 
-
-
-
-
-
-
-
-
-
-
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
+%% Old remnant code
  
 % % baseline voltage
 % % I'm gonna set this up myself by choosing random values. The dataset from
