0fb3e2a4842fe1884ac2dc31cd7ae05e040095bf
Abhishek Bhardwaj <ab2824484@gmail.com>
2022-02-01 20:22:01 +1100

    stuff

--- Simple_Example.m
+++ Simple_Example.m
@@ -6,50 +6,32 @@ set(0,'DefaultFigureWindowStyle','docked')
 %% REES model parameters and such
 
 % Customers, time steps, supply point numbers
-N = 23; % 600 is too much for the purpose of demonstration, there are 29 
-        % supply points with 6 having 0 customers, so I will put 1 customer
-        % on each supply point
+N = 1; 
 T = 48;
-K = 29;
+K = 1;
 
-% power profile vectors (empty for now)
-% maybe I dont need these here...eh whatever, I'll come back to it
-% p = zeros(T,N);
-% q = zeros(T,N);
-% s = zeros(2*K*T, N);
-% u = [p;q;s];
 
-% Stacking design parameters into row vectors, each elements corresponds to
-% customer i
-% setting to zero for now, will fill in soon
-
-% Changing these to simplest possible scenario.
-a = 0; %randi([10,14],1,N); % arrival time 5pm-7pm
-d = T; %randi([38,42],1,N); % departure time 7am-9am
+arrival = 0; 
+departure = T; 
 
 capacities = [42,21,24,20,30,16,23,16.5,28,60,90,75];
 b = zeros(1,N); 
 for i=1:N
     b(i) = capacities(randi([1,12]));
 end
-s_bar = 10; % same for all customers %*ones(1,N); % inverter capacity
-sig_hat = (0.3+0.2*rand(1,N)).*b; % initial SoC
+s_bar = 10; 
+sig_hat = (0.3+0.2*rand(1,N)).*b;
 sig_u = 0.85*b; % max SoC
 sig_l = 0.2*b;  % min SoC
-sig_star = zeros(1,N); % target SoC - this isnt listed in the paper, so I 
-                       % will set this to between sig_hat and sig_upper,
-                       % rounded to the nearest integer
+sig_star = zeros(1,N); 
 for i=1:N
     sig_star(i) = ceil(sig_hat(i) + (sig_u(i)-sig_hat(i))*rand);
 end
 p_u = 6.6;  % max charge rate
 p_l = -6.6; % min charge rate
 
-% One of the places where these get used, Nanduni has missed, so I will set
-% both to 1, to see if Yalmip will work.
 mu_u = 1; %0.9; % charge efficiency
 mu_l = 1; %1.1; % discharge efficiency
-
 
 % variables defined for linear systems
 alpha_l = sig_l - sig_hat;
@@ -73,64 +55,15 @@ kappa = 5e-4; %zeros(1,N);
 
 % Matrix Theta - this has size 29x600
 
-theta = zeros(29, N);
+theta = 1;
 
-% Below comment was for 600 customers, I am changing to now 23.
-% % The only way I can make sense of the numbers in Nanduni's paper is if
-% % each supply point has its own number of customers. So each customer is
-% % essentially only connected to a single phase...
-% % yeah this incrementation and labelling of customers is a bit confusing...
 
-theta(4, 1) = 1;
-theta(5, 2) = 1;
-theta(6, 3) = 1;
-theta(7, 4) = 1;
-theta(8, 5) = 1;
-theta(9, 6) = 1;
-theta(10, 7) = 1;
-theta(11, 8) = 1;
-theta(12, 9) = 1;
-theta(13, 10) = 1;
-theta(17, 11) = 1;
-theta(18, 12) = 1;
-theta(19, 13) = 1;
-theta(20, 14) = 1;
-theta(21, 15) = 1;
-theta(22, 16) = 1;
-theta(23, 17) = 1;
-theta(24, 18) = 1;
-theta(25, 19) = 1; 
-theta(26, 20) = 1; 
-theta(27, 21) = 1;
-theta(28, 22) = 1;
-theta(29, 23) = 1;
 
-% theta(4,1:26) = 1;
-% theta(5,27:52) = 1;
-% theta(6,53:78) = 1;
-% theta(7,79:104) = 1;
-% theta(8,105:130) = 1;
-% theta(9,131:156) = 1;
-% theta(10,157:182) = 1;
-% theta(11, 183:208) = 1;
-% theta(12, 209:234) = 1;
-% theta(13, 235:260) = 1;
-% theta(17, 261:286) = 1;
-% theta(18, 287:312) = 1;
-% theta(19, 313:338) = 1;
-% theta(20, 339:364) = 1;
-% theta(21, 365:390) = 1;
-% theta(22, 391:416) = 1;
-% theta(23, 417:442) = 1;
-% theta(24, 443:468) = 1;
-% theta(25, 469:495) = 1; % Supply point 10 which has 27 customers
-% theta(26, 496:522) = 1; % Supply point 11 which has 27 customers
-% theta(27, 523:548) = 1;
-% theta(28, 549:574) = 1;
-% theta(29, 575:600) = 1;
+% % Loading R and X
+% load('RX.mat');
 
-% Loading R and X
-load('RX.mat');
+R = 2*real(Z_601(1,1)*2000);
+X = 2*imag(Z_601(1,1)*2000);
 
 % matrices D and E
 D = -R*theta;
@@ -198,16 +131,14 @@ for j=1:T
     end
 end
 
-% figure()
-% for i=1:29
-%     plot(xq, V_baseline(i,:), 'k-o')
-%     ylim([0.9,1.1])
-%     hold on
-%     grid on
-%     set(gca,'ytick',[0.0:0.01:1.1])
-% end
-% plot(xq, 1.046*ones(1,48), 'r-')
-% plot(xq, 0.954*ones(1,48), 'r-')
+figure()
+plot(xq, V_baseline, 'k-o')
+ylim([0.9,1.1])
+hold on
+grid on
+set(gca,'ytick',[0.0:0.01:1.1])
+plot(xq, 1.046*ones(1,48), 'r-')
+plot(xq, 0.954*ones(1,48), 'r-')
 
 % The plot seem okay-ish for these baseline voltages. Eyeballing it, they
 % seem roughly similar to Nanduni's plot, so I'll go with this.
@@ -259,12 +190,12 @@ end
 
 %% ADMM
 
-iteration = 20;
+iteration = 100;
 lambda = zeros(2*K*T,N);       % primal value for each customer
 lambda_state = zeros(2*K*T,N); % state value for each customer, the neighbors states
                       % can be found from different columns of this.
 
-c_stepsize = 0.05;
+c_stepsize = 1;
 alpha = 0;
 rho=0;
 lambda_step = {}; % cell to hold each iteration value of the primal
@@ -276,38 +207,12 @@ U = zeros(2*(T+K*T),N);
 
 transmission = zeros(N,iteration);
 
-
-I = eye(T,T);
-O = ones(T,1);
-Z = zeros(T,1);
-
-% Building vector mu, of charge efficiencies
-mu = ones(T,1); %zeros(T,1)
-%         for j=1:T
-%             if p(j)>=0
-%                 mu(j)=mu_u;
-%             else
-%                 mu(j)=mu_l;
-%             end
-%         end
-% Building matrix M which bounds charge rates
-M = tril(ones(T,T));
-% for j=1:T
-%     for l=1:T
-%         if j>=l
-%             M(j,l) = mu(l);
-%         else
-%             M(j,l)=0;
-%         end
-%     end
-% end
-
 for k=1:iteration
     disp(k)
     % looping over customers
-    parfor i=1:N
+    for i=1:N
         
-%         disp([k, i])
+
         
         % Building the sum of difference between self state and neighbors
         % states
@@ -327,27 +232,54 @@ for k=1:iteration
         p = x(1:T);
         q = x(T+1:2*T);
         s = x(2*T+1:end);
-%         
-%         % sanity check
-%         if ~size(p,1)==size(q,1)
-%             error("sizes don't match for p and q")
-%         end
+        
+        % sanity check
+        if ~size(p,1)==size(q,1)
+            error("sizes don't match for p and q")
+        end
         
         %% constraints - inverter capacity
         
-        constraints = [p.^2 + q.^2 <= s_bar*ones(T,1)];
+        constraints = [];
         %inv_cap = s_bar(i)^2;
-%         for j=1:T
-%             constraints = [constraints, p(j)^2+q(j)^2 <= s_bar]; %inv_cap];
-%         end
+        for j=1:T
+            constraints = [constraints, p(j)^2+q(j)^2 <= s_bar]; %inv_cap];
+        end
         
         %% Constraints building other stuff
+        
+        I = eye(T,T);
+        O = ones(T,1);
+        Z = zeros(T,1);
+        
+        
+        % Building vector mu, of charge efficiencies
+        mu = ones(T,1); %zeros(T,1)
+%         for j=1:T
+%             if p(j)>=0
+%                 mu(j)=mu_u;
+%             else
+%                 mu(j)=mu_l;
+%             end
+%         end
 
+        % Building matrix M which bounds charge rates
+        M = zeros(T,T);
+        for j=1:T
+            for l=1:T
+                if j>=l
+                    M(j,l) = mu(l);
+                else
+                    M(j,l)=0;
+                end
+            end
+        end
+        
         constraints = [constraints, M*p <= alpha_u(i)*O, alpha_l(i)*O <= M*p];
         
         constraints = [constraints, p_l*O <= p, p<= p_u*O];
         
-        constraints = [constraints, O'*p >= e(i)];
+        constraints = [constraints, O'*p == e(i)];
         
         % this part is not needed when I set a=0, d=T.
 %         % Building availibility matrix L
@@ -372,10 +304,9 @@ for k=1:iteration
         constraints = [constraints, zeros(length(s),1) <= s]; %, s<=w];
         %% Updating u_n with optimize and Mosek
         
-        obj = eta'*p + kappa*(p'*p);
-        extr_obj = (1/(4*c_stepsize*n_deg(i)))*norm( Psi{i}*x - (1/N)*w - nu(:,i) + c_stepsize*temp )^2  ; 
+        obj = eta'*p + kappa*p'*p;
         opt = sdpsettings('verbose',0);
-        optimize(constraints, obj+extr_obj, opt);
+        optimize(constraints, obj, opt);
         u = value(x);
         U(:,i) = u;
         
@@ -393,18 +324,23 @@ for k=1:iteration
 %         lambda(:,i) = fminsearch(fun, zeros(p,1));
 %         
         
-%         %% Transmission Step
-%         % Computing the difference between current state and primal update
-%         xi(:,i) = lambda_state(:,i) - lambda(:,i); 
-%         % Transmission loop
-%         if ( norm(xi(:,i))^2 - alpha*(rho^k) )>=0
-%             lambda_state(:,i) = lambda(:,i);
-%             transmission(i,k) = 1;
-%         end
+        %% Transmission Step
+        % Computing the difference between current state and primal update
+        xi(:,i) = lambda_state(:,i) - lambda(:,i); 
+        %disp(xi(:,i)')
+        % Transmission loop
+        if ( norm(xi(:,i))^2 - alpha*(rho^k) )>=0
+            %disp(norm(xi(:,i))^2 - alpha*(rho^k))
+            %disp("true")
+            lambda_state(:,i) = lambda(:,i);
+            transmission(i,k) = 1;
+        else
+            %disp("false")
+        end
     end
     
     %% Dual variable update
-    parfor i=1:N
+    for i=1:N
         
         % building the sum of the differences between self state and
         % neighbors current states
@@ -435,28 +371,9 @@ V_soln = V_baseline(:) + temp;
 
 V_soln_reshaped = reshape(V_soln, [K,T]);
 
-% V_soln = zeros(K,T);
-% padding = zeros(T,1);
-% p_soln = [padding, padding, padding, U(1:T,1:10), padding, padding, padding, U(1:T,11:end)];
-% q_soln = [padding, padding, padding, U(T+1:2*T,1:10), padding, padding, padding, U(T+1:2*T,11:end)];
-% 
-% 
-% for i=1:T
-%     V_soln(:,i) = V_base(:,i) - R*p_soln(i,:)' - X*q_soln(i,:)';
-% end
 
 %% Plots
 
-% figure()
-% for i=1:29
-%     plot(xq, V_baseline(i,:), 'k-o')
-%     ylim([0.9,1.1])
-%     hold on
-%     grid on
-%     set(gca,'ytick',[0.0:0.01:1.1])
-% end
-% plot(xq, 1.046*ones(1,48), 'r-')
-% plot(xq, 0.954*ones(1,48), 'r-')
 figure()
 for i=1:K
     plot(V_baseline(i,:), 'ro--')
